d5ca0cffea81a78667ae904d878b8324aa452536
Michael Finch <mikefinch330@gmail.com>
2021-05-04 21:59:16 +0000

    deploy web app

---
+++ web_app_deployment/nba_salary_app.py
@@ -0,0 +1,126 @@
+import os
+import pickle
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+url = ""
+
+@app.route("/", methods=["GET"])
+def index():
+    return_string = "<h1>NBA Salary Predictor</h1>" + "\n" + "<h2>Estimate how Much Money an Nba Player Should Make Based on PER, Games, and Win Shares</h2>" "\n" + "<h3>Query Strings Should Look Something Like This: predict?name=NAME&per=0.0&games=0&ws=0.0</h3>"
+    return return_string, 200
+@app.route("/predict", methods=["get"])
+def predict():
+    name = per = request.args.get("name", "")
+    per = request.args.get("per", "")
+    games = request.args.get("games", "")
+    ws = request.args.get("ws", "")
+    per = float(per)
+    games = float(games)
+    ws = float(ws)
+    per = categorical_per(per)
+    games = categorical_games(games)
+    ws = categorical_ws(ws)
+    prediction = predict_interviews_well([per, games, ws])
+    if prediction is not None: 
+        salary_range = get_salary_range(prediction)
+        pred_string = name + salary_range 
+        result = {"prediction": pred_string}
+        return jsonify(result), 200
+    else: 
+        return "Error making prediction", 400
+def tdidt_predict(header, tree, instance):
+    info_type = tree[0]
+    if info_type == "Attribute":
+        attribute_index = header.index(tree[1])
+        instance_value = instance[attribute_index]
+        for i in range(2, len(tree)):
+            value_list = tree[i]
+            if value_list[1] == instance_value: 
+                return tdidt_predict(header, value_list[2], instance)
+    else: 
+        return tree[1]
+
+def predict_interviews_well(instance):
+    infile = open("tree.p", "rb")
+    header, tree = pickle.load(infile)
+    infile.close()
+    print("header:", header)
+    print("tree:", tree)
+    try: 
+        return tdidt_predict(header, tree, instance)
+    except: 
+        return None 
+def get_salary_range(prediction):
+    if prediction == 1:
+        return " should make under $1,609,676.10 per year."
+    elif prediction == 2:
+        return " should make between $1,609,676.10 and $3,149,352.20 per year."
+    elif prediction == 3: 
+        return " should make between $3,149,352.20 and $4,689,028.30 per year."
+    elif prediction == 4: 
+        return " should make between $4,689,028.30 and $6,228,704.40 per year."
+    elif prediction == 5: 
+        return " should make between $6,228,704.40 and $7,768,380.50 per year."
+    elif prediction == 6: 
+        return " should make between $7,768,380.50 and $9,308,056.60 per year."
+    elif prediction == 7: 
+        return " should make between $9,308,056.60 and $10,847,732.70 per year."
+    elif prediction == 8: 
+        return " should make between $10,847,732.70 and $12,387,408.80 per year."
+    elif prediction == 9: 
+        return " should make between $12,387,408.80 and $13,927,084.90 per year."
+    elif prediction == 10: 
+        return " should make over $13,927,084.90 per year."
+    return ""
+def categorical_games(games):
+    if games < 296.6:
+        return 1
+    elif games >= 296.6 and games < 428.2:
+        return 2
+    elif games >= 428.2 and games < 559.8:
+        return 3
+    elif games >= 559.8 and games < 691.4:
+        return 4
+    elif games >= 691.4 and games < 823.0:
+        return 5
+    elif games >= 823.0 and games < 954.6:
+        return 6
+    elif games >= 954.6 and games < 1086.2:
+        return 7
+    elif games >= 1086.2 and games < 1217.8:
+        return 8
+    elif games >= 1217.8 and games < 1349.4:
+        return 9
+    elif games > 1349.4:
+        return 10
+    return 0
+def categorical_per(per):
+    if per < 11.08:
+        return 1
+    elif per >= 11.08 and per < 15.16:
+        return 2
+    elif per >= 15.16 and per < 19.24:
+        return 3
+    elif per >= 19.24 and per < 23.32:
+        return 4
+    elif per > 23.32:
+        return 5
+    return 0
+def categorical_ws(ws):
+    if ws < 45.64:
+        return 1
+    elif ws >= 45.64 and ws < 92.88:
+        return 2
+    elif ws >= 92.88 and ws < 140.12:
+        return 3
+    elif ws >= 140.12 and ws < 187.36:
+        return 4
+    elif ws > 187.36:
+        return 5
+    return 0
+if __name__ == "__main__":
+    port = os.environ.get("PORT", 5000)
+    app.run(debug=False, host="0.0.0.0", port=port) #set debug to false for production
---
+++ web_app_deployment/salary_pickler.py
@@ -0,0 +1,10 @@
+import pickle 
+
+header = ["per", "games", "ws"]
+salary_tree = ['Attribute', 'games', ['Value', 1, ['Leaf', 1, 141, 771]], ['Value', 2, ['Attribute', 'per', ['Value', 1, ['Leaf', 1, 22, 104]], ['Value', 2, ['Leaf', 1, 60, 104]], ['Value', 3, ['Leaf', 1, 18, 104]], ['Value', 4, ['Leaf', 6, 2, 104]], ['Value', 5, ['Leaf', 4, 2, 104]]]], ['Value', 3, ['Attribute', 'per', ['Value', 1, ['Leaf', 1, 20, 136]], ['Value', 2, ['Leaf', 1, 79, 136]], ['Value', 3, ['Leaf', 5, 0, 27]], ['Value', 4, ['Leaf', 8, 0, 9]], ['Value', 5, ['Leaf', 8, 1, 136]]]], ['Value', 4, ['Leaf', 2, 0, 121]], ['Value', 5, ['Attribute', 'per', ['Value', 1, ['Leaf', 1, 5, 98]], ['Value', 2, ['Leaf', 4, 0, 63]], ['Value', 3, ['Leaf', 3, 23, 98]], ['Value', 4, ['Leaf', 3, 4, 98]], ['Value', 5, ['Leaf', 7, 3, 98]]]], ['Value', 6, ['Attribute', 'per', ['Value', 1, ['Leaf', 2, 2, 87]], ['Value', 2, ['Leaf', 2, 0, 41]], ['Value', 3, ['Leaf', 6, 0, 36]], ['Value', 4, ['Leaf', 7, 0, 6]], ['Value', 5, ['Leaf', 10, 2, 87]]]], ['Value', 7, ['Attribute', 'per', ['Value', 1, ['Leaf', 1, 1, 45]], ['Value', 2, ['Leaf', 4, 0, 17]], ['Value', 3, ['Leaf', 5, 0, 22]], ['Value', 4, ['Leaf', 10, 0, 3]], ['Value', 5, ['Leaf', 5, 0, 2]]]], ['Value', 8, ['Leaf', 4, 0, 21, ['Value', 2, ['Leaf', 4, 7, 21]], ['Value', 3, ['Leaf', 3, 0, 10]], ['Value', 4, ['Leaf', 5, 0, 3]], ['Value', 5, ['Leaf', 10, 1, 21]]]], ['Value', 9, ['Leaf', 2, 0, 11]], ['Value', 10, ['Leaf', 6, 0, 7, ['Value', 2, ['Leaf', 3, 1, 7]], ['Value', 3, ['Leaf', 6, 3, 7]], ['Value', 4, ['Leaf', 4, 1, 7]], ['Value', 5, ['Leaf', 8, 2, 7]]]]]
+
+packaged_object = [header, salary_tree]
+# pickle packaged_object
+outfile = open("tree.p", "wb")
+pickle.dump(packaged_object, outfile)
+outfile.close()
